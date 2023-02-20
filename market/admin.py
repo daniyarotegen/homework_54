d3dd38399c0d91099f92c64764c99222972298cc
Daniyar Otegen <daniyarotegen@gmail.com>
2023-02-20 20:20:39 +0600

    Added CategoryAdmin and ProductAdmin classes, made migrations, created superuser

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from market.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    list_filter = ('id', 'name', 'description')
+    search_fields = ('id', 'name', 'description')
+    fields = ('id', 'name', 'description')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'added_datetime', 'cost')
+    list_filter = ('id', 'name', 'category')
+    search_fields = ('id', 'name', 'description')
+    readonly_fields = ('id', 'added_datetime')
+    fields = ('id', 'name', 'category', 'added_datetime', 'cost')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
