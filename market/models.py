b01fb4dc246eb21acabf67291fa7af2427b1e0a5
Daniyar Otegen <daniyarotegen@gmail.com>
2023-02-20 20:08:27 +0600

    Added database and models

--- market/models.py
+++ market/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Name')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Description')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Name')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Description')
+    category = models.ForeignKey(
+        Category,
+        null=False,
+        blank=False,
+        related_name='product',
+        verbose_name='Product',
+        on_delete=models.CASCADE)
+    added_datetime = models.DateTimeField(auto_now_add=True, verbose_name='Added datetime')
+    cost = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False, verbose_name='Cost')
+    image = models.URLField(blank=True, verbose_name='Image URL')
+
+    def __str__(self):
+        return self.name
--- source/settings.py
+++ source/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'source.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lesson_54_hw',
+        'USER': 'macbookair',
+        'PASSWORD': '618396',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
